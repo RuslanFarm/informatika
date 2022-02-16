6b7c758e8142c6651cc29326f130ea79ff16de8d
RuslanFarm <94564710+RuslanFarm@users.noreply.github.com>
2022-02-16 15:21:46 +0300

    Update zadanie6.py

--- zadanie6.py
+++ zadanie6.py
@@ -1,12 +1,14 @@
-j = ''
-s = 6
-n = 1
-for x in range(0, 30000, 1):
-    while s * (x // 15) < 300:
-        x += 1
+count=0
+for i in range(0, 30000, 1):
+    x=i
+    s=6*(x//15)
+    n=1
+    while s < 300:
+
         s += 18
         n *= 2
-    if n <= 500:
-        print(n)
+    if 2 <= n <= 500:
+        count+=1
+print(count)
 
 
