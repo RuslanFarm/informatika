e73ca840edbaa9e95f288e620835718f2b0ce6aa
RuslanFarm <94564710+RuslanFarm@users.noreply.github.com>
2022-02-16 15:22:14 +0300

    Update asd.py

--- asd.py
+++ asd.py
@@ -3,17 +3,15 @@ def N_in_6(n):
     while n != 0:
         s += str(n % 6)
         n //= 6
-    s = int(s[::-1] + s[0])
-    print(s)
+    s = int((s[::-1] + s[0]), 6)
+    # s=int(s,6)
     s2 = (bin(s) + bin(s)[-1])[2:]
-    print(s2)
-
-    s3 = ''
-    while s2 != 0:
-        s3 += str(s2 % 10)
-        s2 //= 10
-    s3 = int(s3[::-1] + s3[0])
-    print(s3)
+    # s3 = int((s3[::-1] + s3[0]), 2)
+    return int(s2, 2)
 
 
-N_in_6(13)
+# N_in_6(13)
+for i in range(1, 1000):
+    f = N_in_6(i)
+    if f < 344:
+        print(f)
