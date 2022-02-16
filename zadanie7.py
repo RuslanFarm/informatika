20d6024862356f433e6065dac83fadc6a26f0e3d
RuslanFarm <94564710+RuslanFarm@users.noreply.github.com>
2022-02-16 15:25:44 +0300

    Update zadanie7.py

--- zadanie7.py
+++ zadanie7.py
@@ -1,9 +1,11 @@
-s = ''
-x = int(input())
-a = 125 ** 200 - 5 ** x + 74
-while a != 0:
-    s += str(a % 5)
-    a //= 5
-s = s[::-1]
-if s.count('4') == 100:
-    print(x)
+for i in range(1000):
+    s = ''
+    x = i
+    a = 125 ** 200 - 5 ** x + 74
+    while a != 0:
+        s += str(a % 5)
+        a //= 5
+    s = s[::-1]
+    if s.count('4') == 100:
+        print(x)
+        
